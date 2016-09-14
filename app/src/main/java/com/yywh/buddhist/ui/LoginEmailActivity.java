b760ced040b4cb42c58cfdeafd45726b0b8ad133
yioump <yioump@163.com>
2016-09-14 17:57:00 +0800

    登录模块优化

--- app/src/main/java/com/yywh/buddhist/ui/LoginEmailActivity.java
+++ app/src/main/java/com/yywh/buddhist/ui/LoginEmailActivity.java
@@ -1,5 +1,6 @@
 package com.yywh.buddhist.ui;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -20,12 +21,18 @@ import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.yywh.buddhist.MyApp;
 import com.yywh.buddhist.R;
+import com.yywh.buddhist.http.XCallback;
+
+import org.xutils.http.RequestParams;
+import org.xutils.x;
 
 import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import butterknife.BindView;
+import butterknife.OnClick;
 
 /**
  * 邮箱登录
@@ -39,6 +46,8 @@ public class LoginEmailActivity extends BaseActivity {
     @BindView(R.id.btn_email_login)
     Button BtnEmailLogin;
 
+    private ProgressDialog progressDialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -51,92 +60,102 @@ public class LoginEmailActivity extends BaseActivity {
 
     @Override
     protected void init() {
-        BtnEmailLogin.setOnClickListener(new View.OnClickListener() {
-            private String password;
-            private String email;
+//        BtnEmailLogin.setOnClickListener(new View.OnClickListener() {
+//            private String password;
+//            private String email;
+//
+//            @Override
+//            public void onClick(View v) {
+//                email = LoginEmail.getText().toString().trim();
+//                password = LoginPwd.getText().toString().trim();
+//
+//                if (!(TextUtils.isEmpty(email) || TextUtils.isEmpty(password))) {
+//                    // 执行登录逻辑
+//                    Login();
+//                } else {
+//                    Toast.makeText(LoginEmailActivity.this, "邮箱或密码不能为空", Toast.LENGTH_SHORT).show();
+//                }
+//            }
+//
+//            private void Login() {
+//                String url = "http://wx.yywhsh.com/login/login";
+//                RequestQueue mRequestQueue = Volley.newRequestQueue(getApplicationContext());
+//                StringRequest postRequest = new StringRequest(Request.Method.POST, url,
+//                        new Response.Listener<String>() {
+//                            @Override
+//                            public void onResponse(String response) {
+//                                if (progressDialog != null && progressDialog.isShowing()) progressDialog.dismiss();
+//
+//                                // response
+//                                Log.d("Response", response);
+//                                System.out.println("Response" + response);
+//                                Intent intent = new Intent(LoginEmailActivity.this, MainActivity.class);
+//
+//                                startActivity(intent);
+//
+//                            }
+//                        },
+//                        new Response.ErrorListener() {
+//                            @Override
+//                            public void onErrorResponse(VolleyError error) {
+//
+//                            }
+//                        }
+//
+//
+//                ) {
+//                    @Override
+//                    protected Map<String, String> getParams() {
+//                        Map<String, String> params = new HashMap<String, String>();
+//                        params.put("userName", email);
+//                        params.put("email", email);
+//                        params.put("pwd", password);
+//
+//                        return params;
+//                    }
+//
+//                    @Override
+//                    protected Response<String> parseNetworkResponse(NetworkResponse response) {
+//                        try {
+//
+//                            Map<String, String> responseHeaders = response.headers;
+//                            String rawCookies = responseHeaders.get("Set-Cookie");
+//                            System.out.println("rawCookies===" + rawCookies);
+//                            String dataString = new String(response.data, "UTF-8");
+//                            MyApp.setCookieValue(rawCookies);
+//                            return Response.success(dataString, HttpHeaderParser.parseCacheHeaders(response));
+//                        } catch (UnsupportedEncodingException e) {
+//                            return Response.error(new ParseError(e));
+//                        }
+//                    }
+//                };
+//                mRequestQueue.add(postRequest);
+//
+//                progressDialog = new ProgressDialog(LoginEmailActivity.this);
+//                progressDialog.setMessage("登录中...");
+//                progressDialog.show();
+//            }
+//
+//        });
+    }
+
+    @OnClick({R.id.btn_email_login})
+    public void doClick() {
+        RequestParams requestParams = new RequestParams("http://wx.yywhsh.com/login/login");
+        requestParams.addParameter("userName", LoginEmail.getText().toString());
+        x.http().post(requestParams, new XCallback<String, String>(this) {
+            @Override
+            public void onSuccess(String result) {
+
+            }
 
             @Override
-            public void onClick(View v) {
-                email = LoginEmail.getText().toString().trim();
-                password = LoginPwd.getText().toString().trim();
-
-                if (!(TextUtils.isEmpty(email) || TextUtils.isEmpty(password))) {
-                    // 执行登录逻辑
-                    Login();
-                } else {
-                    Toast.makeText(LoginEmailActivity.this, "邮箱或密码不能为空", Toast.LENGTH_SHORT).show();
-                }
+            public String prepare(String rawData) {
+                return null;
             }
-
-            private void Login() {
-                String url = "http://wx.yywhsh.com/login/login";
-                RequestQueue mRequestQueue = Volley.newRequestQueue(getApplicationContext());
-                StringRequest postRequest = new StringRequest(Request.Method.POST, url,
-                        new Response.Listener<String>() {
-                            @Override
-                            public void onResponse(String response) {
-                                // response
-                                Log.d("Response", response);
-                                System.out.println("Response" + response);
-                                Intent intent = new Intent(LoginEmailActivity.this, MainActivity.class);
-
-                                startActivity(intent);
-
-                            }
-                        },
-                        new Response.ErrorListener() {
-                            @Override
-                            public void onErrorResponse(VolleyError error) {
-                                // error
-                                Log.d("Error.Response", error.getMessage());
-                                System.out.println("Error.Response" + error.getMessage());
-                            }
-                        }
-
-
-                ) {
-                    @Override
-                    protected Map<String, String> getParams() {
-                        Map<String, String> params = new HashMap<String, String>();
-                        params.put("userName", email);
-                        params.put("email", email);
-                        params.put("pwd", password);
-
-                        return params;
-                    }
-
-                    @Override
-                    protected Response<String> parseNetworkResponse(NetworkResponse response) {
-                        try {
-
-                            Map<String, String> responseHeaders = response.headers;
-                            String rawCookies = responseHeaders.get("Set-Cookie");
-                            System.out.println("rawCookies==="+rawCookies);
-                            String dataString = new String(response.data, "UTF-8");
-                            MyApp.setCookieValue(rawCookies);
-                            return Response.success(dataString, HttpHeaderParser.parseCacheHeaders(response));
-                        } catch (UnsupportedEncodingException e) {
-                            return Response.error(new ParseError(e));
-                        }
-                    }
-                };
-                mRequestQueue.add(postRequest);
-
-            }
-
         });
-    }
+        List headers = requestParams.getHeaders();
 
-    @Override
-    public void onStart() {
-        super.onStart();
-
-
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
     }
 
 }
